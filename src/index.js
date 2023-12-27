d90082020c7948d4334bd61487bf2ec9bb438718
OlgaGresko <122181692+OlgaGresko@users.noreply.github.com>
2023-12-28 01:09:05 +0200

    created view.js and its main features

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import Game from "./game";
+import View from "./view";
+
+const root = document.querySelector("#root");
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
+
 
 window.game = game;
+window.view = view;
 
-console.log('game', game)
+view.renderPlayfield(game.playfield)
