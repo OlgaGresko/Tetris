135ab1ad11701ef6a7f5fdfeacc5bd6d61067ac6
OlgaGresko <122181692+OlgaGresko@users.noreply.github.com>
2024-01-03 19:44:34 +0200

    added methods to game.js & listeners to index.js

--- src/view.js
+++ src/view.js
@@ -1,36 +1,55 @@
 export default class View {
-    constructor(element, width, height, rows, columns) {
-        this.element = element;
-        this.width = width;
-        this.height = height;
+  constructor(element, width, height, rows, columns) {
+    this.element = element;
+    this.width = width;
+    this.height = height;
 
-        this.canvas = document.createElement("canvas");
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
-        this.context = this.canvas.getContext("2d");
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.context = this.canvas.getContext('2d');
 
-        this.blockWidth = this.width / columns;
-        this.blockHeight = this.height / rows;
+    this.blockWidth = this.width / columns;
+    this.blockHeight = this.height / rows;
 
-        this.element.appendChild(this.canvas);
-    }
+    this.element.appendChild(this.canvas);
+  }
 
-    renderPlayfield(playfield) {
-        for (let y = 0; y < playfield.length; y++) {
-            const line = playfield[y];
+  render({ playfield }) {
+    this.clearScreen();
+    this.renderPlayfield(playfield);
+  }
 
-            for (let x = 0; x < line.length; x++) {
-                const block = line[x];
+  clearScreen() {
+    this.context.clearRect(0, 0, this.width, this.height);
+  }
 
-                if (block) {
-                    this.context.fillStyle = "red";
-                    this.context.strokeStyle = "black";
-                    this.context.lineWidth = 2;
+  renderPlayfield(playfield) {
+    for (let y = 0; y < playfield.length; y++) {
+      const line = playfield[y];
 
-                    this.context.fillRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight);
-                }
-            }
-            
+      for (let x = 0; x < line.length; x++) {
+        const block = line[x];
+
+        if (block) {
+          this.renderBlock(
+            x * this.blockWidth,
+            y * this.blockHeight,
+            this.blockWidth,
+            this.blockHeight,
+            'red'
+          );
         }
+      }
     }
+  }
+
+  renderBlock(x, y, width, height, color) {
+    this.context.fillStyle = color;
+    this.context.strokeStyle = 'black';
+    this.context.lineWidth = 2;
+
+    this.context.fillRect(x, y, width, height);
+    this.context.strokeRect(x, y, width, height);
+  }
 }
